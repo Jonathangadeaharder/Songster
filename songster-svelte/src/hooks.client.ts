92aa9c0db850c2ddebbdbc4a02fca490fb4573fd
Jonathan Gadea Harder <jonathangadeaharder@Jonathans-MacBook-Pro.local>
2026-05-02 14:05:11 +0200

    fix: address PR review comments for highlight telemetry

--- songster-svelte/src/hooks.client.ts
+++ songster-svelte/src/hooks.client.ts
@@ -1,16 +1,17 @@
 import { H } from 'highlight.run';
-import { HIGHLIGHT_PROJECT_ID, HIGHLIGHT_BACKEND_URL } from '$lib/config/highlight';
+import {
+	HIGHLIGHT_PROJECT_ID,
+	HIGHLIGHT_BACKEND_URL,
+	highlightConfig,
+} from '$lib/config/highlight';
 
 H.init(HIGHLIGHT_PROJECT_ID, {
 	serviceName: 'songster',
-	backendUrl: HIGHLIGHT_BACKEND_URL,
+	environment: highlightConfig.environment,
 	tracingOrigins: true,
 	networkRecording: {
-		enabled: true,
-		recordHeadersAndBody: true,
-		urlBlocklist: [
-			'https://www.googleapis.com/identitytoolkit',
-			'https://securetoken.googleapis.com',
-		],
+		enabled: false,
+		recordHeadersAndBody: false,
 	},
+	backendUrl: HIGHLIGHT_BACKEND_URL,
 });
